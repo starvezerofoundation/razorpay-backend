a5285b5f6b6d86d6fbb5a710173c77987b60e8de
starvezerofoundation <starvezerofoundation@gmail.com>
2026-01-23 06:28:43 +0530

    server.js

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
+function allowCors(res) {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+}
+
+export default async function handler(req, res) {
+    allowCors(res)
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end()
+    }
+
+    // ... your existing code below (DO NOT change logic)
+}
+
 import Razorpay from "razorpay"
 
 /* -------------------- CORS HELPER -------------------- */
